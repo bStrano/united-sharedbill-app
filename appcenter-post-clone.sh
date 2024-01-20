8004ad1b181a9475f7ec8050be9dd474910f15f7
Bruno Strano <bruno_strano@hotmail.com>
2024-01-20 19:15:28 -0300

    🔧 Changing submodule to use https instead of ssh because of Appcenter

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-#ADD Bitbucket fingerprint to known_hosts
 mkdir -p ~/.ssh
-echo "Adding bitbucket to ssh known hosts"
+echo "Adding github to ssh known hosts"
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 #ADD SSH key to the image
-echo "Adding bitbucket SSH key"
+echo "Adding github SSH key"
 echo $GITHUB_SSH_KEY | base64 -D > ~/.ssh/github-ssh
 chmod 600 ~/.ssh/github-ssh
 ssh-add ~/.ssh/github-ssh
