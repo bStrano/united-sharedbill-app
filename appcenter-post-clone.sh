48ee4af8c2cf654fd13d9a78727b0eccb8e8d38c
Bruno Strano <bruno_strano@hotmail.com>
2024-01-20 19:34:19 -0300

    🐛 ...

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
-mkdir -p ~/.ssh
-echo "Adding github to ssh known hosts"
-ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-#ADD SSH key to the image
-echo "Adding github SSH key"
-echo $GITHUB_SSH_KEY | base64 -D > ~/.ssh/github-ssh
-chmod 600 ~/.ssh/github-ssh
-ssh-add ~/.ssh/github-ssh
+##!/usr/bin/env bash
+#mkdir -p ~/.ssh
+#echo "Adding github to ssh known hosts"
+#ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+##ADD SSH key to the image
+#echo "Adding github SSH key"
+#echo $GITHUB_SSH_KEY | base64 -D > ~/.ssh/github-ssh
+#chmod 600 ~/.ssh/github-ssh
+#ssh-add ~/.ssh/github-ssh
